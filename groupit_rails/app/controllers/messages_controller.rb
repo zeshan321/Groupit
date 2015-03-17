0b51c4952c4dc4c274d97e3c4b8813930e78b671
Edward Guo <edwardplguo@gmail.com>
2015-03-17 10:44:42 -0400

    Authenticate users access to public/private channel

--- groupit_rails/app/controllers/messages_controller.rb
+++ groupit_rails/app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
 		message.group_id = params[:group_id]
 		message.user = current_user
 		if message.save
-			channel_name = "g"+ params[:group_id].to_s
+			channel_name = "G"+ params[:group_id].to_s
 			message_body = {:text => message.text, :author_name => current_user.name, :author_id =>current_user.id}
 			WebsocketRails[channel_name].trigger(:new, message_body, :namespace => :messages)
       render plain:'OK'
