0483fdd1b5d9be043a6be7b1997b1512a8d60470
Zeshan Aslam <zeshan12aslam@gmail.com>
2015-03-24 22:11:33 -0400

    Soft input
    Do not open keyboard when opening message activity.

--- app/src/main/java/com/groupit/MessageActivity.java
+++ app/src/main/java/com/groupit/MessageActivity.java
@@ -18,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -62,6 +63,9 @@ public class MessageActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         TextView tv = (TextView) findViewById(R.id.groupTitle);
         tv.setText(groupName);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
 
         isLooking = true;
         con = this;
