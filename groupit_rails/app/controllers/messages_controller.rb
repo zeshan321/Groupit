89934cb04143c4ec180998bfd23dd35e783883e8
Edward Guo <edwardplguo@gmail.com>
2015-03-29 15:32:32 -0400

    Escape HTML before message send through Websocket

--- groupit_rails/app/controllers/messages_controller.rb
+++ groupit_rails/app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   include ApiHelper
+  include ERB::Util
   skip_before_action :verify_authenticity_token, only: [:api_create]
   before_action :authenticate_api, only: [:api_create]
 
@@ -10,7 +11,7 @@ class MessagesController < ApplicationController
   def create
     create_message params[:text], params[:image], params[:group_id]
   end
-  
+
   def create_message text, image, group_id
 		message = Message.new
 		message.text = text
@@ -20,7 +21,7 @@ class MessagesController < ApplicationController
     if user_allow_access?(message.group)
   		if message.save
   			channel_name = "G"+ group_id.to_s
-  			message_body = {:text => message.text, :author_name => current_user.name, :author_id =>current_user.id}
+  			message_body = {:text => html_escape(message.text), :author_name => current_user.name, :author_id =>current_user.id}
         if message.has_image
           img_size = message.limit_img_tag_size(500)
           message_body[:image] = {:path => message.image.url, :width => img_size[0], :height => img_size[1]}
