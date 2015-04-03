f9d46efd6116a2e9b70d82f8546a22b4826e0859
Zeshan Aslam <zeshan12aslam@gmail.com>
2015-04-02 21:38:21 -0400

    On boot
    Start app on boot.

--- app/src/main/java/com/groupit/BootReceiver.java
+++ app/src/main/java/com/groupit/BootReceiver.java
@@ -10,8 +10,8 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             new GroupHandler(context).loadGroupMem(context);
-            //Intent serviceIntent = new Intent(context, ClientMessage.class);
-            //context.startService(serviceIntent);
+            Intent serviceIntent = new Intent(context, MessageService.class);
+            context.startService(serviceIntent);
             MessageService.tempCon = context;
 
             GroupActivity.ID = new UserData(context).getID();
